4e48ebe66688eddf8e0a7086fabc1285e3eed8be
MohabMowafy <mowafymohab@gmail.com>
2025-08-08 06:21:43 +0300

    Create project

---
+++ FoodApp/FoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by مهاب موافي on 8/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
